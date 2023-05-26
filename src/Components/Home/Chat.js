31c9094c6c0d9b7fd0a48272c72756543642c051
impaaras <impaaras00@gmail.com>
2023-05-26 23:37:03 +0530

    chat section done

--- src/Components/Home/Chat.js
+++ src/Components/Home/Chat.js
@@ -1,10 +1,65 @@
-import { collection, onSnapshot } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import React, { useEffect, useState } from "react";
-import { db } from "../../Firebase";
-import { Button } from "antd";
+import { auth, db } from "../../Firebase";
+import { Button, Input } from "antd";
+import { useParams } from "react-router-dom";
 
 const Chat = () => {
   const [users, setUsers] = useState([]);
+
+  const [messages, setMessages] = useState([]);
+
+  const workspaceId = useParams();
+  const [messageInput, setMessageInput] = useState("");
+
+  const chatsCollectionRef = collection(
+    db,
+    "workspaces",
+    workspaceId.toString(),
+    "chats"
+  );
+
+  // Function to add a new chat message
+  const sendMessage = async () => {
+    if (messageInput.trim() === "") return;
+
+    try {
+      // Add the new message to the Firestore collection
+      await addDoc(chatsCollectionRef, {
+        message: messageInput,
+        sender: auth.currentUser.uid,
+        timestamp: new Date(),
+      });
+
+      // Clear the message input field
+      setMessageInput("");
+    } catch (error) {
+      console.error("Error sending message:", error);
+    }
+  };
+
+  useEffect(() => {
+    // Retrieve and listen for new messages using onSnapshot
+    const unsubscribe = onSnapshot(
+      query(chatsCollectionRef, orderBy("timestamp", "asc"), limit(50)),
+      (snapshot) => {
+        const chatMessages = [];
+        snapshot.forEach((doc) => {
+          chatMessages.push(doc.data());
+        });
+        setMessages(chatMessages);
+      }
+    );
+
+    return () => unsubscribe();
+  }, []);
 
   useEffect(() => {
     const usersCollectionRef = collection(db, "users");
@@ -116,7 +171,51 @@ const Chat = () => {
             ))}
           </div>
         </div>
-        <div style={{ width: "85%", backgroundColor: "rgba(0,0,0,0.7)" }}></div>
+        <div
+          style={{
+            width: "85%",
+            backgroundColor: "rgba(0,0,0,0.7)",
+            display: "flex",
+            flexDirection: "column",
+            height: "100%",
+            padding: "10px",
+          }}
+        >
+          <div style={{ flex: "1", overflowY: "auto" }}>
+            {messages &&
+              messages.map((message, index) => (
+                <p
+                  key={index}
+                  style={{
+                    textAlign:
+                      message.sender === auth.currentUser.uid
+                        ? "left"
+                        : "right",
+                    backgroundColor:
+                      message.sender === auth.currentUser.uid ? "#DDD" : "#FFF",
+                    color: "#000",
+                    width: "fit-content",
+                    padding: "5px",
+                    margin: "5px",
+                    borderRadius: "5px",
+                  }}
+                >
+                  {message.message}
+                </p>
+              ))}
+          </div>
+          <Input
+            type="text"
+            placeholder="Type something..."
+            value={messageInput}
+            onChange={(e) => setMessageInput(e.target.value)}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                sendMessage();
+              }
+            }}
+          />
+        </div>
       </div>
     </div>
   );

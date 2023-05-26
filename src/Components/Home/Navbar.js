fd93f312a6b4cbfa75f9e0ee08090d35417fce14
impaaras <impaaras00@gmail.com>
2023-05-26 11:18:56 +0530

    add tomato

--- src/Components/Home/Navbar.js
+++ src/Components/Home/Navbar.js
@@ -25,103 +25,103 @@ import {
 const Navbar = () => {
   const { shift, setShift } = useContext(GlobalStateContext);
 
-  // const [time, setTime] = useState(
-  //   parseInt(localStorage.getItem("time")) || 30
-  // );
-  // const [isActive, setIsActive] = useState(
-  //   localStorage.getItem("isActive") === "true" || true
-  // );
-  // const [isBreak, setIsBreak] = useState(
-  //   localStorage.getItem("isBreak") === "true" || false
-  // );
-  // const [showModal, setShowModal] = useState(
-  //   localStorage.getItem("showModal") === "true" || false
-  // );
+  const [time, setTime] = useState(
+    parseInt(localStorage.getItem("time")) || 30
+  );
+  const [isActive, setIsActive] = useState(
+    localStorage.getItem("isActive") === "true" || true
+  );
+  const [isBreak, setIsBreak] = useState(
+    localStorage.getItem("isBreak") === "true" || false
+  );
+  const [showModal, setShowModal] = useState(
+    localStorage.getItem("showModal") === "true" || false
+  );
 
-  // function toggle() {
-  //   setIsActive(!isActive);
-  // }
+  function toggle() {
+    setIsActive(!isActive);
+  }
 
-  // function reset() {
-  //   setIsActive(false);
-  //   setTime(30);
-  //   setIsBreak(false);
-  //   setShowModal(false);
-  // }
+  function reset() {
+    setIsActive(false);
+    setTime(30);
+    setIsBreak(false);
+    setShowModal(false);
+  }
 
-  // useEffect(() => {
-  //   localStorage.setItem("isActive", isActive);
-  //   localStorage.setItem("isBreak", isBreak);
-  //   localStorage.setItem("showModal", showModal);
-  //   localStorage.setItem("time", time);
-  // }, [isActive, isBreak, showModal, time]);
+  useEffect(() => {
+    localStorage.setItem("isActive", isActive);
+    localStorage.setItem("isBreak", isBreak);
+    localStorage.setItem("showModal", showModal);
+    localStorage.setItem("time", time);
+  }, [isActive, isBreak, showModal, time]);
 
-  // useEffect(() => {
-  //   let interval = null;
-  //   let timeout = null;
+  useEffect(() => {
+    let interval = null;
+    let timeout = null;
 
-  //   // If the timer is active and it reaches 0, set break to true and start the break timer
-  //   if (isActive && time === 0) {
-  //     setIsActive(false);
-  //     setIsBreak(true);
-  //     setShowModal(true);
-  //     setTime(10);
-  //     timeout = setTimeout(() => {
-  //       setShowModal(false);
-  //       setIsActive(true);
-  //       setIsBreak(false);
-  //       setTime(30);
-  //     }, 10000);
-  //   }
+    // If the timer is active and it reaches 0, set break to true and start the break timer
+    if (isActive && time === 0) {
+      setIsActive(false);
+      setIsBreak(true);
+      setShowModal(true);
+      setTime(10);
+      timeout = setTimeout(() => {
+        setShowModal(false);
+        setIsActive(true);
+        setIsBreak(false);
+        setTime(30);
+      }, 10000);
+    }
 
-  //   // If the timer is not active and the break timer reaches 0, start the timer again
-  //   if (!isActive && isBreak && time === 0) {
-  //     setShowModal(false);
-  //     setIsActive(true);
-  //     setIsBreak(false);
-  //     setTime(30);
-  //   }
+    // If the timer is not active and the break timer reaches 0, start the timer again
+    if (!isActive && isBreak && time === 0) {
+      setShowModal(false);
+      setIsActive(true);
+      setIsBreak(false);
+      setTime(30);
+    }
 
-  //   // Start the timer when it is active
-  //   if (isActive) {
-  //     interval = setInterval(() => {
-  //       setTime((time) => time - 1);
-  //     }, 1000);
-  //   } else {
-  //     clearInterval(interval);
-  //   }
+    // Start the timer when it is active
+    if (isActive) {
+      interval = setInterval(() => {
+        setTime((time) => time - 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
 
-  //   // Clear the interval and timeout when the component unmounts
-  //   return () => {
-  //     clearInterval(interval);
-  //     clearTimeout(timeout);
-  //   };
-  // }, [isActive, isBreak, time]);
+    // Clear the interval and timeout when the component unmounts
+    return () => {
+      clearInterval(interval);
+      clearTimeout(timeout);
+    };
+  }, [isActive, isBreak, time]);
 
-  // useEffect(() => {
-  //   let timeout = null;
-  //   if (showModal) {
-  //     timeout = setTimeout(() => {
-  //       setShowModal(false);
-  //       setIsBreak(false);
-  //       setTime(30);
-  //       setIsActive(true);
-  //     }, 10000);
-  //   }
-  //   return () => clearTimeout(timeout);
-  // }, [showModal]);
+  useEffect(() => {
+    let timeout = null;
+    if (showModal) {
+      timeout = setTimeout(() => {
+        setShowModal(false);
+        setIsBreak(false);
+        setTime(30);
+        setIsActive(true);
+      }, 10000);
+    }
+    return () => clearTimeout(timeout);
+  }, [showModal]);
 
-  // const timerStyle = {
-  //   color: isBreak ? "red" : isActive ? "red" : "white",
-  // };
+  const timerStyle = {
+    color: isBreak ? "red" : isActive ? "red" : "white",
+  };
 
-  // const iconStyle = {
-  //   color: isBreak ? "red" : isActive ? "red" : "white",
-  // };
+  const iconStyle = {
+    color: isBreak ? "red" : isActive ? "red" : "white",
+  };
 
-  // const breakStyle = {
-  //   color: isBreak ? "green" : "white",
-  // };
+  const breakStyle = {
+    color: isBreak ? "green" : "white",
+  };
   const { visible, setVisible } = useContext(GlobalStateContext);
 
   const changeloc = useNavigate();
@@ -273,12 +273,12 @@ const Navbar = () => {
           <Button className="button_exit" onClick={() => setAdd(true)}>
             <BsPlusLg className="exit__icon" />
           </Button>
-          {/* <Button className="button_exit">
+          <Button className="button_exit">
             <p>
               {Math.floor(time / 60)}:{("0" + (time % 60)).slice(-2)}
             </p>
             <GiTomato onClick={toggle} className="tommato" />
-          </Button> */}
+          </Button>
           <Button
             className="button_exit"
             style={{ fontSize: "18px", color: "#FFF" }}
